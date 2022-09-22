31066898f90b7a89ec678f8384b2e53a612b5559
marouaneXan <marouanezahaoui835@gmail.com>
2022-09-22 17:27:12 +0100

    create function getRandomStudent

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-//Varibles
+//letibles
 let nameInput = document.getElementById("name");
 let sjtInput = document.getElementById("sjt_veille");
 let btnAdd = document.getElementById("add");
 let results = [];
+let listR = [];
+let D = moment().format("YYYY-MM-DD");
 let table1 = document.querySelector("#table1");
 let table2 = document.querySelector("#table2");
 let id = 0;
-let counter=1
+let counter = 1;
 
 //Add Student with subjects in localStorage
 const displayStudents = () => {
   let nom = nameInput.value;
   let sujet_veille = sjtInput.value;
-  let template = `<tr class='bg-white border-b dark:bg-gray-800 dark:border-gray-700'><th scope='row' class='py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white'>${nom}</th><td class='py-4 px-6'>${sujet_veille}</td></tr>`;
-  if (nom && sujet_veille) {
-    table1.innerHTML += template;
-  }else{
-    alert("Entrer tout les champs")
-  }
   results.push({
     id: id++,
     nom,
     sujet_veille,
   });
-  return results
+  let template = `<tr data-id=${id} class='bg-white border-b dark:bg-gray-800 dark:border-gray-700'><th scope='row' class='py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white'>${nom}</th><td class='py-4 px-6'>${sujet_veille}</td></tr>`;
+  if (nom && sujet_veille) {
+    table1.innerHTML += template;
+  } else {
+    alert("Entrer tout les champs");
+  }
+  return results;
 };
 btnAdd.addEventListener("click", displayStudents);
 
 //Skipping wekeend
-const skippingWekeend = (date, days) => {
+function skippingWekeend(date, days) {
   let d = moment(new Date(date)).add(Math.floor(days / 5) * 7, "d");
   let remaining = days % 5;
   while (remaining) {
@@ -36,22 +38,22 @@ const skippingWekeend = (date, days) => {
     if (d.day() !== 0 && d.day() !== 6) remaining--;
   }
   return d.format("YYYY-MM-DD");
-};
+}
 
 //Get Random Student
 
 const getRandomStudent = () => {
   let randomStudent = results[Math.floor(Math.random() * results.length)];
   results = results.filter((item) => item.id !== randomStudent.id);
-  let template = `<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-  <th
-    scope="row"
-    class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-  >
-    ${results.nom}
-  </th>
-  <td class="py-4 px-6">Sliver</td>
-  <td class="py-4 px-6">22/22/2022</td>
+  document.querySelector(`[data-id="${randomStudent.id + 1}"]`).remove();
+
+  let day = skippingWekeend(D, counter++);
+  let template = `<tr id="list" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+  <td id="nameone" scope="row"
+  class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">${randomStudent.nom}</td>
+  <td id="subj" class="py-4 px-6">${randomStudent.sujet_veille}</td>
+  <td id="subj" class="py-4 px-6">${day}</td>
 </tr>`;
-table2.innerHTML+=template
+  table2.innerHTML += template;
+  console.log(randomStudent);
 };
